8daa989d927576be8008d5b3b8cde250478a88fc
iza16 <borowieckaizabela@gmail.com>
2015-07-02 16:09:52 +0000

    fin

--- app.js
+++ app.js
@@ -4,9 +4,7 @@ var crypto = require('crypto');
 var fs = require('fs');
 //zawiera funkcje pomocnicze generowania skrótów robienia z jonson obiektu ...
 var helpers = require("./helpers");
-//accessKeyId ... klucze do amazona 
 AWS.config.loadFromPath('./config.json');
-//obiekt dla instancji S3 z aws-sdk
 var s3 = new AWS.S3();
 //plik z linkiem do kolejki
 var APP_CONFIG_FILE = "./app.json";
@@ -30,8 +28,8 @@ var myServer = function(){
 		AttributeNames: ['All'],
 		MaxNumberOfMessages: 1,
 		MessageAttributeNames: ['key','bucket'],
-		VisibilityTimeout: 10,//na tyle sec nie widac jej w kolejce
-		WaitTimeSeconds: 0//to na 0 
+		VisibilityTimeout: 10,
+		WaitTimeSeconds: 0 
 	};
 	
 	//odbiera wiadomość
@@ -40,9 +38,8 @@ var myServer = function(){
 		console.log(err, err.stack); // an error occurred
 	}
 	else {
-		//console.log(JSON.stringify(data, null, 4));
 		
-		//Czy jest jakaś wiadomość
+		//sprawdzenie czy jest jakaś wiadomość
 		if(!data.Messages) {
 			console.log("Brak wiadomości w kolejce");
 		} else {
@@ -50,12 +47,9 @@ var myServer = function(){
 			//pobranie danych z body wiadomosci w kolejce i zrobienie z nich tablicy
 			//handler do ussunięcia wiadomości z kolejki
 			var ReceiptHandle_forDelete = data.Messages[0].ReceiptHandle;
-			//{bucket, key}
+		
 			var messageinfo = JSON.parse(data.Messages[0].Body);
 			console.log("Otrzymano wiadomosc: bucket - "+messageinfo.bucket+", key - "+messageinfo.key);
-			
-			//to samo co wyzej tylko pobiera dane z metadanych a nie z body
-			//var messageinfo = { "bucket":data.Messages[0].MessageAttributes.bucket.StringValue,"key":data.Messages[0].MessageAttributes.key.StringValue}console.log(messageinfo.bucket);
 				
 			//parametry do pobrania pliku (obiektu)
 			var params2 = {
@@ -63,30 +57,31 @@ var myServer = function(){
 				Key: messageinfo.key
 			};
 			//zapisujemy plik z s3 na dysku
-			var file = require('fs').createWriteStream('tmp/'+messageinfo.key.substring(10));
+			var file = require('fs').createWriteStream('tmp/'+messageinfo.key.substring(8));
 			var requestt = s3.getObject(params2).createReadStream().pipe(file);
 			//po zapisie na dysk
 			requestt.on('finish', function (){
-				console.log('jestem tu po zapisaniu pliku na dysk');
+				console.log('plik zapisany na dysk');
 
 				//zmieniamy coś w pliku
-				gm('tmp/'+messageinfo.key.substring(10))
+				gm('tmp/'+messageinfo.key.substring(8))
 				.colorize(200, 200, 256)
 				.autoOrient()
-				.write('tmp/'+messageinfo.key.substring(10), function (err) {
+				.write('tmp/'+messageinfo.key.substring(8), function (err) {
 				if (err) {
 					console.log(err);
 				}
+				
 				//po udanej zmienie w pliku
 				else {
 					console.log(' udalosie przetworzuc plik');	
 					
 					//wrzucamy na s3 nowy plik
-					var fileStream = require('fs').createReadStream('tmp/'+messageinfo.key.substring(10));
+					var fileStream = require('fs').createReadStream('tmp/'+messageinfo.key.substring(8));
 					fileStream.on('open', function () {
 						var paramsu = {
 							Bucket: messageinfo.bucket,
-							Key: 'przetworzoneObrazki/'+messageinfo.key.substring(10),
+							Key: 'przetworzoneObrazki/'+messageinfo.key.substring(8),
 							ACL: 'public-read',
 							Body: fileStream,
 						};
@@ -98,25 +93,7 @@ var myServer = function(){
 							console.log(datau);
 							console.log('zuploadowano');
 							
-							
-							//zmieniamy info w bazie że już przerobiony plik
-							var paramsdb = {
-								Attributes: [
-									{ 
-									Name: messageinfo.key, 
-									Value: "yes", 
-									Replace: true
-									}
-								],
-								DomainName: "borowieckaStatus", 
-								ItemName: 'ITEM001'
-							};
-							simpledb.putAttributes(paramsdb, function(err, datass) {
-							if (err) {
-								console.log('Blad zapisu do bazy'+err, err.stack);
-							}
-							else {
-								console.log("Zapisano do bazy");
+					
 								//usuwanie wiadomosci z kolejki
 								var params = {
 								  QueueUrl: linkKolejki,
@@ -127,8 +104,7 @@ var myServer = function(){
 								  else     console.log("Usunieto wiadomosc z kolejki: "+data);           // successful response
 								});
 							}
-							});
-						}
+						
 						}
 					);
 					});	
